e6ac7c72298bab0e196d69faa0e7af2ad7444dc6
Andrey Grudtsin <agrudtsin@gmail.com>
2015-02-09 01:27:45 +0200

    Восстановил базовую работу с фразами

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,17 @@
 'use strict';
-angular.module('grammatist', ['ui.router','ngResource','firebase','model.users','model.categories','model.phrases'])
-  .config(function ($stateProvider, $urlRouterProvider) {
+
+angular.module('grammatist',
+  ['ui.router',
+    'ngResource',
+    'firebase',
+    'model.users',
+    'model.categories',
+    'model.phrases'])
+.factory('dataProvider', ['$http', DataProvider])
+.factory('phrasesLegacy', ['$http', 'dataProvider', Phrases])
+.directive('ngEnter', ngEnter)
+.directive('googleTts', ['$document',googleTts])
+.config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('home', {
         url: '/',
@@ -40,3 +51,154 @@ angular.module('grammatist', ['ui.router','ngResource','firebase','model.users',
 
     $urlRouterProvider.otherwise('/');
   });
+
+
+
+function DataProvider($http) {
+  return {
+    getCategories: function () {
+      return $http.get('phrases/phrases.json');
+    },
+    getPhrasesByID: function (id) {
+      return $http.get('phrases/' + id + '.json');
+    },
+    getPhrasesArrayById: function (id) {
+      $http.get('phrases/' + id + '.json').
+        success(function (data) {
+          return data;
+        }).
+        error(function () {
+          return [];
+        }
+      );
+    }
+  }
+}
+function Phrases($http, dataProvider) {
+  var phrasesObj = {
+    currentPhrase: {
+      text: {
+        ru: "Стандартная фраза",
+        en: "Default phrase"
+      }
+    },
+    deleteLastWord: function (userText) {
+      var lastSpacePosition = s.rtrim(userText, " ").lastIndexOf(' ');
+      if (lastSpacePosition > 0) return _.initial(s.words(userText," ")).join(" ") + " ";
+      return "";
+    },
+    deleteLastSpace: function (text) {
+      return s.rtrim(text, ' ');
+    },
+    buildUnderlinedTextByString: function (userText) {
+      var underlinedText = "";
+      for (var curChar = 0; curChar < userText.length; curChar += 1) {
+        if (userText[curChar] == " ") {
+          underlinedText += " ";
+        } else {
+          underlinedText += this.underlineOrPunctuationМark(userText[curChar]);
+        }
+      }
+      return underlinedText;
+    },
+    underlineOrPunctuationМark: function (character) {
+      var punctuationMarks = [',', '.', ';', ':', '?', '!'];
+      if (_.contains(punctuationMarks, character)) {
+        return character
+      } else {
+        return "_";
+      }
+    },
+
+    buildRedText: function (targetPhrase, userText) {
+      var redText = "";
+      for (var i = 0; i < userText.length; i += 1) {
+        if (targetPhrase[i] !== userText[i]) {
+          redText += userText[i];
+        } else {
+          redText += " ";
+        }
+      }
+      return redText;
+
+    },
+    buildGrayText:function(targetPhrase, userText){
+      var grayText = "";
+      grayText =_.reduce(s.trim(userText), function(memo,context){return memo+=" "},"");//replace all chars by spaces
+      grayText += " "+ s.words(targetPhrase)[s.words(userText).length];// add next word from targetPhrase
+      if(s.words(userText).length == 0) grayText = grayText.slice(1);//remove first space if no words in user text
+      return grayText;
+    },
+    buildWrongSpaces:function(targetPhrase, userText){
+      var wrongSpaces = "";
+      for (var i = 0; i < userText.length; i += 1) {
+        if(userText[i] == " " && userText[i] != targetPhrase[i]){
+          wrongSpaces += "█";
+        } else {
+          wrongSpaces += " ";
+        };
+      };
+      return wrongSpaces;
+    },
+    isContainErrors: function (redText) {
+      return ' ' != _.uniq(redText).toString() && '' != _.uniq(redText).toString()
+    },
+    isWordFinished: function(targetPhrase, userText){
+      if(userText=="") return true;
+      var targetPhraseWords = s.words(targetPhrase);
+      return _.reduce(s.words(userText), function(result, value, key){
+        return result && (value == targetPhraseWords[key])
+      },true);
+    },
+    cropUserTextIfContainErrors: function (targetPhrase, userText) {
+      var isContainErrors = this.isContainErrors(this.buildRedText(targetPhrase, userText));
+      var isContainErrorsInPreviousWord = this.isContainErrors(this.buildRedText(targetPhrase, this.deleteLastWord(userText)));
+      if (isContainErrors && isContainErrorsInPreviousWord) {
+        return this.deleteLastWord(userText)
+      } else {
+        return userText;
+      }
+    },
+    getCurrent: function () {
+      return this.currentPhrase;
+    },
+    categories: [],
+    phrasesList: []
+  };
+
+  dataProvider.getCategories().success(function (data) {
+    phrasesObj.categories = data;
+  });
+
+  return phrasesObj;
+}
+function ngEnter() {
+  return function (scope, element, attrs) {
+    element.bind("keydown keypress", function (event) {
+      if (event.which === 13) {
+        scope.$apply(function () {
+          scope.$eval(attrs.ngEnter);
+        });
+        event.preventDefault();
+      }
+      if (event.which === 32) {
+        scope.$apply(function () {
+          scope.$eval("onUserTextSpace()");
+        });
+        event.preventDefault();
+      }
+    });
+  };
+}
+function googleTts($document){
+  return function(scope, element, attrs) {
+    var section   = $document[0].getElementById("googleTTS");
+    var frame = $document[0].createElement("iframe" );
+    section.appendChild( frame );
+    // http://jsfiddle.net/royriojas/SE6ET/
+    scope.$on('TTS', function(event, userText) {
+      frame.src = 'http://translate.google.com/translate_tts?ie=utf-8&tl=en&q='+userText.split(' ').join('+');
+      console.log('Play', frame.src);
+    });
+  };
+}
--- src/app/phrase/phrase.controller.js
+++ src/app/phrase/phrase.controller.js
@@ -1,23 +1,151 @@
 'use strict';
 
 angular.module('grammatist')
-  .controller('PhraseCtrl', function (Categories, Phrases, PhrasesUtils, $stateParams, $state) {
+  .controller('PhraseCtrl', function (Categories, Phrases, PhrasesUtils, $stateParams, $state, phrasesLegacy, $timeout) {
     var ctrl = this;
 
+    ctrl.currentPhrase = {
+      textEn:"",
+      textRus:""
+    };
+    ctrl.phrases = [];
     ctrl.stateParamms = $stateParams;
+
     Phrases.get($stateParams.categoryID).$promise
       .then(function(phrases){
         ctrl.phrases = PhrasesUtils.toArray(phrases);
         if(isInvalidPhraseID()){
           ctrl.goToNextPhrase();
         }
-
         ctrl.currentPhrase = ctrl.phrases[$stateParams.phraseID];
       });
 
-    ctrl.fire = function(){
-      $stateParams.phraseID =  +$stateParams.phraseID  + 1;
-      $state.go('phrase',$stateParams, {});
+    ctrl.clearUserText = function(){
+      ctrl.redText = "";
+      ctrl.userText = "";
+      ctrl.redText = "";
+      ctrl.grayText = "";
+      ctrl.underlinesText = phrasesLegacy.buildUnderlinedTextByString(ctrl.currentPhrase.textEn);
+      ctrl.wrongSpaces = "";
+    };
+    ctrl.clearUserText();
+    ctrl.onUserTextSpace = function(){
+      ctrl.userText += " ";
+      ctrl.onUserTextChange();
+    };
+
+    ctrl.onUserTextChange = function () {
+      //Если пользователь исправил ошибку отменим таймауты автоматической подсказки
+      if (ctrl.incorrectCharTimeout || ctrl.isPhrasesEqual()){
+        $timeout.cancel(ctrl.incorrectWordTimeout);
+        $timeout.cancel(ctrl.incorrectCharTimeout);
+      }
+
+      //Не даем вносить больше символов чем необходимо
+      if(ctrl.currentPhrase.textEn.length < ctrl.userText.length){
+        ctrl.userText = ctrl.userText.slice(0,ctrl.userText.length-1);
+      }
+
+
+      if(isContainErrors(ctrl.currentPhrase.textEn, ctrl.userText)){
+        ctrl.redText = getBlankString(ctrl.userText.length - 1) + _.last(ctrl.userText);
+        ctrl.userText = _.initial(ctrl.userText).join('');
+
+        ctrl.incorrectCharTimeout = $timeout(function () {
+          ctrl.grayText = getBlankString(ctrl.userText.length) + ctrl.currentPhrase.textEn[ctrl.userText.length];
+          ctrl.redText = "";
+        },2000);
+
+        ctrl.incorrectWordTimeout = $timeout(function () {
+          var correctUserWords =s.words(ctrl.userText);
+          if(_.last(ctrl.userText) == ' '){
+            correctUserWords.push(s.words(ctrl.currentPhrase.textEn)[correctUserWords.length]);
+          } else {
+            correctUserWords =_.initial(correctUserWords);
+            correctUserWords.push(s.words(ctrl.currentPhrase.textEn)[correctUserWords.length]);
+          }
+
+          ctrl.grayText = correctUserWords.join(' ');
+          ctrl.redText = "";
+        },4000);
+      } else {
+        ctrl.redText = "";
+      }
+
+      function isContainErrors(targetPhrase, userPhrase){
+        return !s.startsWith(targetPhrase, userPhrase);
+      }
+      function getBlankString(length){
+        return s.repeat(' ', length);
+      }
+    };
+
+    ctrl.isPhrasesEqual = function () {
+      return ctrl.currentPhrase.textEn == ctrl.userText;
+    };
+    ctrl.isContainErrors = function () {
+      return phrasesLegacy.isContainErrors(ctrl.redText);
+    };
+    ctrl.isContainErrorSpaces = function(){
+      return phrasesLegacy.isContainErrors(ctrl.wrongSpaces);
+    };
+    ctrl.isWordFinished = function () {
+      return phrasesLegacy.isWordFinished(ctrl.currentPhrase.textEn, ctrl.userText);
+    };
+    ctrl.buildRedText = function () {
+      ctrl.redText = phrasesLegacy.buildRedText(ctrl.currentPhrase.textEn, ctrl.userText);
+      ctrl.wrongSpaces = phrasesLegacy.buildWrongSpaces(ctrl.currentPhrase.textEn, ctrl.userText);
+    };
+
+    ctrl.onUserTextEnter = function () {
+      if (useEnterInTheEndOfPhrase()) return;
+      //if (useEntersToUnderlineText()) return;
+      useEnterToShowGrayedText();
+
+      function useEnterToShowGrayedText() {
+        if (ctrl.isContainErrorSpaces()){
+          ctrl.userText = _.str.rtrim(ctrl.userText);
+        }
+        if (ctrl.isContainErrors() || !ctrl.isWordFinished()) {
+          ctrl.userText = phrasesLegacy.deleteLastWord(ctrl.userText);
+        }
+        ctrl.grayText = phrasesLegacy.buildGrayText(ctrl.currentPhrase.textEn, ctrl.userText);
+        ctrl.buildRedText();
+      }
+      //function useEntersToUnderlineText() {
+      //    if (isAllPhraseAlreadyUnderlined()) return false; //phrase already underined
+      //    ctrl.underlinesText = phrasesLegacy.buildUnderlinedTextByString(ctrl.currentPhrase.text.en);
+      //    return true;
+      //}
+      //function isAllPhraseAlreadyUnderlined() {
+      //    return ctrl.currentPhrase.text.en.length == ctrl.underlinesText.length
+      //}
+      function useEnterInTheEndOfPhrase() {
+
+
+        //var sound = new Audio("http://translate.google.com/translate_tts?tl=en&q="+
+        //    ctrl.currentPhrase.text.en.split(' ').join('+'));
+        //console.log(sound.load());
+        //console.log(sound.play());
+
+        if (ctrl.isPhrasesEqual()) {
+          if (ctrl.currentPhrase != _.last(ctrl.phrasesList)) setNextPhrase();
+          //else setNextCategory();
+          return true;
+        }
+        return false;
+
+        function setNextPhrase() {
+          ctrl.phraseOnChange(ctrl.phrasesList[_.indexOf(ctrl.phrasesList, ctrl.currentPhrase) + 1])
+        }
+        //function setNextCategory() {
+        //  if (ctrl.categories != _.last(ctrl.currentCategory)) {
+        //    ctrl.categoryOnChange(ctrl.categories[_.indexOf(ctrl.categories, ctrl.currentCategory) + 1]);
+        //  } else {
+        //    ctrl.categoryOnChange(_.first(ctrl.categories));
+        //  }
+        //}
+      }
     };
 
     function isInvalidPhraseID() {
@@ -47,3 +175,4 @@ angular.module('grammatist')
     };
 
   });
+
